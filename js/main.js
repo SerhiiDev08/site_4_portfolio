1bbce3cba852dca269dc6ecfadaa156555e12a15
SerhiiHaker <sergeytrader080808@gmail.com>
2023-03-28 00:57:12 +0300

    add media burger

--- js/main.js
+++ js/main.js
@@ -6,20 +6,28 @@ let links = document.querySelectorAll('.burger__menu-item');
 let button = document.querySelector('.burger__menu-btn');
 
 burger.addEventListener('click', () => {
-	menu.classList.add('active')
+	menu.classList.add('active');
+
+	if (menu.classList.contains('active')) {
+		document.body.style.overflow = 'hidden';
+	}
 })
 
 btnClose.addEventListener('click', () => {
 	menu.classList.remove('active')
+	document.body.style.overflow = 'scroll';
 })
 overlay.addEventListener('click', () => {
 	menu.classList.remove('active')
+	document.body.style.overflow = 'scroll';
 })
 for (let link of links) {
 	link.addEventListener('click', () => {
 		menu.classList.remove('active')
+		document.body.style.overflow = 'scroll';
 	})
 }
 button.addEventListener('click', () => {
 	menu.classList.remove('active')
+	document.body.style.overflow = 'scroll';
 })
